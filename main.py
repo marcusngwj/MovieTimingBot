230733615ee0bb8a424371c78418ce0688a98638
Verbena <verbena_94@hotmail.com>
2017-01-21 21:47:36 +0800

    Add the cinema function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 start_message = """ Hello, I'm the Movie Timing Bot! I can give you some usage stats for your stickers. Use these commands to control me:
 
@@ -10,7 +11,10 @@ Movie Timings
 /ratings - get the ratings for a specific movie
 
 /hello - greetings from me! :)
+/help - show the list of commands
 /cancel - cancel the current operation """
+
+cinema_message = "Which cinema do you want to watch at?"
 
 def start(bot, update):
     update.message.reply_text(start_message)
@@ -20,8 +24,22 @@ def hello(bot, update):
         'Hello {}'.format(update.message.from_user.first_name))
 
 def cinema(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+    keyboard = [[InlineKeyboardButton("Cathay Cineplexes", callback_data='Cathay Cineplexes')],
+                [InlineKeyboardButton("Filmgarde Cineplexes", callback_data='Filmgarde Cineplexes')],
+                [InlineKeyboardButton("Golden Village", callback_data='Golden Village')],
+                [InlineKeyboardButton("Shaw Theatres", callback_data='Shaw Theatres')],
+                [InlineKeyboardButton("Any", callback_data='Any')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text(cinema_message, reply_markup=reply_markup)
+
+def button(bot, update):
+    query = update.callback_query
+
+    bot.editMessageText(text="Selected option: %s" % query.data,
+                        chat_id=query.message.chat_id,
+                        message_id=query.message.message_id)
 
 def movie(bot, update):
     update.message.reply_text(
@@ -48,10 +66,12 @@ updater = Updater('321039232:AAH36i7fC0h3W2k9or7-IXDiR9Lp3BmhQZg')
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('cinema', cinema))
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
 updater.dispatcher.add_handler(CommandHandler('movie', movie))
 updater.dispatcher.add_handler(CommandHandler('date', date))
 updater.dispatcher.add_handler(CommandHandler('time', time))
 updater.dispatcher.add_handler(CommandHandler('ratings', ratings))
+updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('cancel', cancel))
 
 updater.start_polling()
